cb8b8bdb1f12b07a55e70526c0bbff7ec9b5f3f4
Kenny Oh <ohdoking@gmail.com>
2018-12-24 12:05:29 +0100

    set oauth server

--- oauth-server/src/main/java/com/ohdoking/oauth/OauthServerApplication.java
+++ oauth-server/src/main/java/com/ohdoking/oauth/OauthServerApplication.java
@@ -1,7 +1,11 @@
 package com.ohdoking.oauth;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,10 +13,8 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootApplication
+@EnableEurekaClient
 @EnableAuthorizationServer
 @EnableResourceServer
 @RestController
